766a2368835ea97a5f2c4a0ad4836f3126557958
Vovdenko Dmitriy <vov-dm-an@yandex-team.ru>
2025-07-04 21:03:57 +0300

    Serial protocol example fixed

--- hydros_serial_protocol/example/src/main.cpp
+++ hydros_serial_protocol/example/src/main.cpp
@@ -27,7 +27,7 @@ public:
     ThreadSecuredMemory() { memory_mutex_ = osMutexNew(nullptr); }
 
 private:
-    uint8_t public_memory_[BUFFER_LENGTH];
+    uint8_t public_memory_[BUFFER_LENGTH] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     osMutexId_t memory_mutex_;
 
